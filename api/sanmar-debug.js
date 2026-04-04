75119ee9b6253cf73dab4b74e29f1bff09068489
jmdm815 <jhairson@buyjmmedia.com>
2026-04-04 01:11:22 -0500

    Update sanmar-debug.js

--- api/sanmar-debug.js
+++ api/sanmar-debug.js
@@ -1,16 +1,36 @@
-// api/sanmar-debug.js
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  const user = process.env.SANMAR_USERNAME;
+  const pass = process.env.SANMAR_PASSWORD;
+
+  // The SanMarWebServicePort only has inventory methods.
+  // Product info uses a different endpoint. Let's try it:
+  // Method: getInventoryQtyForStyleColorSize
+  // args: username, password, style, color, size, sanmarUserRegNum
+  const soap = `<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                  xmlns:web="http://webservice.integration.sanmar.com/">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <web:getInventoryQtyForStyleColorSize>
+      <arg0>${user}</arg0>
+      <arg1>${pass}</arg1>
+      <arg2>PC61</arg2>
+      <arg3>Black</arg3>
+      <arg4>M</arg4>
+      <arg5></arg5>
+    </web:getInventoryQtyForStyleColorSize>
+  </soapenv:Body>
+</soapenv:Envelope>`;
 
   try {
-    // Just fetch the WSDL to see what methods are available
-    const response = await fetch(
-      'https://ws.sanmar.com:8080/SanMarWebService/SanMarWebServicePort?wsdl',
-      { headers: { 'Accept': 'text/xml' } }
-    );
-    const xml = await response.text();
-    // Return first 5000 chars to see operations
-    res.status(200).json({ status: response.status, wsdl: xml.substring(0, 5000) });
+    const r = await fetch('https://ws.sanmar.com:8080/SanMarWebService/SanMarWebServicePort', {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/xml;charset=UTF-8', 'SOAPAction': '""' },
+      body: soap,
+    });
+    const xml = await r.text();
+    res.status(200).json({ status: r.status, preview: xml.substring(0, 4000) });
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
