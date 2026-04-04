ddfea7d722454c0aa76a1c1427dfaa97c13de670
jmdm815 <jhairson@buyjmmedia.com>
2026-04-04 01:24:38 -0500

    Update sanmar-debug.js

--- api/sanmar-debug.js
+++ api/sanmar-debug.js
@@ -3,35 +3,49 @@ export default async function handler(req, res) {
   const acct = process.env.SANMAR_ACCOUNT;
   const user = process.env.SANMAR_USERNAME;
   const pass = process.env.SANMAR_PASSWORD;
-  const { style, color, size } = req.query;
 
-  // Try inventory call with just style, empty color/size to get all
-  const soap = `<?xml version="1.0" encoding="UTF-8"?>
+  const results = {};
+
+  // Try the PromoStandards product info endpoint
+  const psProductSoap = `<?xml version="1.0" encoding="UTF-8"?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-                  xmlns:web="http://webservice.integration.sanmar.com/">
+                  xmlns:ns="http://www.promostandards.org/WSDL/ProductDataService/2.0.0/">
   <soapenv:Header/>
   <soapenv:Body>
-    <web:getInventoryQtyForStyleColorSize>
-      <arg0>${acct}</arg0>
-      <arg1>${user}</arg1>
-      <arg2>${pass}</arg2>
-      <arg3>${style || 'PC61'}</arg3>
-      <arg4>${color || ''}</arg4>
-      <arg5>${size || ''}</arg5>
-      <arg6></arg6>
-    </web:getInventoryQtyForStyleColorSize>
+    <ns:GetProductRequest>
+      <ns:wsVersion>2.0.0</ns:wsVersion>
+      <ns:id>${user}</ns:id>
+      <ns:password>${pass}</ns:password>
+      <ns:localizationCountry>US</ns:localizationCountry>
+      <ns:localizationLanguage>en</ns:localizationLanguage>
+      <ns:productId>PC61</ns:productId>
+      <ns:partId></ns:partId>
+      <ns:colorName></ns:colorName>
+      <ns:ApparelSizeArray></ns:ApparelSizeArray>
+    </ns:GetProductRequest>
   </soapenv:Body>
 </soapenv:Envelope>`;
 
   try {
-    const r = await fetch('https://ws.sanmar.com:8080/SanMarWebService/SanMarWebServicePort', {
-      method: 'POST',
-      headers: { 'Content-Type': 'text/xml;charset=UTF-8', 'SOAPAction': '""' },
-      body: soap,
+    const r = await fetch('https://ws.sanmar.com:8080/promostandards/ProductDataServiceBinding?wsdl', {
+      method: 'GET',
     });
-    const xml = await r.text();
-    res.status(200).json({ status: r.status, preview: xml.substring(0, 5000) });
-  } catch(err) {
-    res.status(500).json({ error: err.message });
+    results.productDataWsdlStatus = r.status;
+    const txt = await r.text();
+    results.productDataWsdlPreview = txt.substring(0, 500);
+  } catch(e) {
+    results.productDataWsdlError = e.message;
   }
+
+  // Also try fetching the product info service WSDL
+  try {
+    const r2 = await fetch('https://ws.sanmar.com:8080/SanMarWebService/SanMarProductInfoServicePort?wsdl');
+    results.productInfoWsdlStatus = r2.status;
+    const txt2 = await r2.text();
+    results.productInfoWsdlPreview = txt2.substring(0, 500);
+  } catch(e) {
+    results.productInfoWsdlError = e.message;
+  }
+
+  res.status(200).json(results);
 }
