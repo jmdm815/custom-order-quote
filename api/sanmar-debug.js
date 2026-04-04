1d018bed4a4f17df49480af0241decb4d186d7f5
jmdm815 <jhairson@buyjmmedia.com>
2026-04-04 01:23:40 -0500

    Update sanmar-debug.js

--- api/sanmar-debug.js
+++ api/sanmar-debug.js
@@ -3,7 +3,9 @@ export default async function handler(req, res) {
   const acct = process.env.SANMAR_ACCOUNT;
   const user = process.env.SANMAR_USERNAME;
   const pass = process.env.SANMAR_PASSWORD;
+  const { style, color, size } = req.query;
 
+  // Try inventory call with just style, empty color/size to get all
   const soap = `<?xml version="1.0" encoding="UTF-8"?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
                   xmlns:web="http://webservice.integration.sanmar.com/">
@@ -13,9 +15,9 @@ export default async function handler(req, res) {
       <arg0>${acct}</arg0>
       <arg1>${user}</arg1>
       <arg2>${pass}</arg2>
-      <arg3>PC61</arg3>
-      <arg4>Black</arg4>
-      <arg5>M</arg5>
+      <arg3>${style || 'PC61'}</arg3>
+      <arg4>${color || ''}</arg4>
+      <arg5>${size || ''}</arg5>
       <arg6></arg6>
     </web:getInventoryQtyForStyleColorSize>
   </soapenv:Body>
@@ -28,11 +30,7 @@ export default async function handler(req, res) {
       body: soap,
     });
     const xml = await r.text();
-    res.status(200).json({
-      creds: { acct: acct ? acct.substring(0,3)+'***' : 'MISSING', user: user ? user.substring(0,3)+'***' : 'MISSING', pass: pass ? 'SET' : 'MISSING' },
-      status: r.status,
-      preview: xml.substring(0, 3000)
-    });
+    res.status(200).json({ status: r.status, preview: xml.substring(0, 5000) });
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
